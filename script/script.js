4a8a5cb2f3b3d6019990d13ddcd33c9d81cfbea2
Icaro-pavani <icaro.pavani79@gmail.com>
2022-01-30 10:13:12 -0300

    troca de let por const e alguns comentarios adicionados

--- script/script.js
+++ script/script.js
@@ -1,21 +1,21 @@
 // Abaixo aloca os elementos de cada item (prato, bebida e sobremesa) em variáveis de vetores
-let myPlate = document.getElementsByClassName('prato'); 
-let myDrink = document.getElementsByClassName('bebida');
-let myDessert = document.getElementsByClassName('sobremesa');
+const myPlate = document.getElementsByClassName('prato'); 
+const myDrink = document.getElementsByClassName('bebida');
+const myDessert = document.getElementsByClassName('sobremesa');
 
 // Abaixo os botòes do HTML são alocas em variáveis
-let botao = document.querySelector('.checar-pedido');
-let botaoCancelar = document.querySelector('.cancelar');
-let botaoPedir = document.querySelector('.pedir');
+const botao = document.querySelector('.checar-pedido');
+const botaoCancelar = document.querySelector('.cancelar');
+const botaoPedir = document.querySelector('.pedir');
 
-// Desabilita o botão para confirmar o pedido
+// Desabilita o botão para confirmar o pedido na tela principal
 botao.disabled = true;
 
 // Definição das variáveis globais
-let pos;    //posições dos dados dentro da tabela da confirmação do pedido
-let total;  //Total do pedido
-let nome;   //Aloca o nome quando pedido no pormpt  
-let endereco;   //Aloca o endereço pedido no pormpt
+let pos = null;    //posições dos dados dentro da tabela da confirmação do pedido
+let total = null;  //Total do pedido
+let nome = null;   //Aloca o nome quando pedido no pormpt  
+let endereco = null;   //Aloca o endereço pedido no pormpt
 
 // Função para remover todos os itens já selcionados da subcategoria (prato, bebida ou sobremesa)
 function removeStyles(arr) {
@@ -26,14 +26,14 @@ function removeStyles(arr) {
 
 /* Função para criar a reação ao clique para todos os elementos das categorias pratos, bebida e sobremesa
 e verifica a seleção de um item de cada categoria para transformar e liberar o botão de confirmação */
-function selecionar(campo) {
+function selecionar(secao) {
     // ativa como se fosse onclick para cada item
-    for (let value of campo) {
-        value.addEventListener('click', function () {
+    for (let item of secao) {
+        item.addEventListener('click', function () {
             if (this.classList.contains('buttonClicked')) {
                 this.classList.toggle('buttonClicked');
             } else {
-                removeStyles(campo);
+                removeStyles(secao);
                 this.classList.add('buttonClicked');
             }
 
@@ -55,6 +55,7 @@ function selecionar(campo) {
 function mandarMensagem() {
     let str = `Olá, gostaria de fazer o pedido:\n- Prato: ${pos[0].innerHTML}\n- Bebida: ${pos[2].innerHTML}\n- Sobremesa: ${pos[4].innerHTML}\nTotal: R$ ${total.toFixed(2)}\n\nNome: ${nome}\nEndereço: ${endereco}`;
     let a = encodeURIComponent(str);
+    // abre uma nova janela para o whats com a mensagem
     window.open("https://wa.me/5519991029727?text=" + a, "_blank");
 }
 
@@ -70,6 +71,7 @@ function checarPedido() {
     pos[3].innerHTML = selecionados[1].querySelector('h4').innerHTML.replace(/[^0-9.,-]+/g, "");
     pos[4].innerHTML = selecionados[2].querySelector('h3').innerHTML;
     pos[5].innerHTML = selecionados[2].querySelector('h4').innerHTML.replace(/[^0-9.,-]+/g, "");
+    // Number() funciona como um parseInt ou ParseFloat
     total = Number(pos[1].innerHTML.replace(',', '.')) + Number(pos[3].innerHTML.replace(',', '.')) + Number(pos[5].innerHTML.replace(',', '.'));
     pos[7].innerHTML = total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }); //toLocaleString transforma o valor direto para o padrão de moeda local
     document.querySelector('.finalizar').classList.add('fechado');
